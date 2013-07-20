d879280b524689d061e1d68612281064d249207d
Daniel Greene <daniel.b.greene@gmail.com>
2013-07-19 23:38:39 -0400

    Initial UI Commit
    UI POC w/ functional admin page

---
+++ earthdynamics/ui/app/app.js
@@ -0,0 +1,41 @@
+﻿
+var app = angular.module('earthDynamicsApp', []);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/',
+            {
+                controller: 'HomeController',
+                templateUrl: '/app/partials/home.html'
+            })
+        .when('/about',
+            {
+                controller: 'AboutController',
+                templateUrl: '/app/partials/about.html'
+            })
+        .when('/contact',
+        {
+            controller: 'ContactController',
+            templateUrl: '/app/partials/contact.html'
+        })
+        .when('/projects',
+        {
+            controller: 'ProjectController',
+            templateUrl: '/app/partials/project.html'
+        })
+        .when('/admin',
+        {
+            controller: 'AdminController',
+            templateUrl: '/app/partials/admin.html'
+        })
+        .when('/admin/projects',
+        {
+            controller: 'ManageProjectController',
+            templateUrl: '/app/partials/manageprojects.html'
+        })
+        .otherwise({ redirectTo: '/' });
+});
+
+
+
+
